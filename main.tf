8f4bb40f9980eb41e4bdf2f30923009a7bccebf6
lambohamp <36923213+lambohamp@users.noreply.github.com>
2019-05-16 16:17:05 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 provider "aws" {
   region = "${var.region}"
 }
-module "EC2" {
-  source = "./modules/EC2"
-  region = "${var.region}"
-  instance_type     = "${var.instance_type}"
-  instance_name     = "${var.instance_name}"
-  ami               = "${var.ami}"
-  vpc_cidr           = "${var.vpc_cidr}"
-  subnet_cidr              = "${var.subnet_cidr}"
-  your_ip = "${var.your_ip}"
-  ec2_port       = "${var.ec2_port}"
-}
 
+module "EC2" {
+  source        = "./modules/EC2"
+  region        = "${var.region}"
+  instance_type = "${var.instance_type}"
+  instance_name = "${var.instance_name}"
+  ami           = "${var.ami}"
+  vpc_cidr      = "${var.vpc_cidr}"
+  subnet_cidr   = "${var.subnet_cidr}"
+  your_ip       = "${var.your_ip}"
+  ec2_port      = "${var.ec2_port}"
+}
