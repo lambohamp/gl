6973c517e1e53ae956c20ef42edbfdd1b89b19a6
lambohamp <dmitriypivnev@gmail.com>
2019-05-24 12:20:30 +0300

    Update

---
+++ terraform/aks/resources.tf
@@ -0,0 +1,22 @@
+resource "azurerm_resource_group" "k8s" {
+  name     = "${var.resource_group_name}"
+  location = "${var.location}"
+}
+
+resource "azurerm_kubernetes_cluster" "k8s" {
+  name                = "${var.cluster_name}"
+  location            = "${azurerm_resource_group.k8s.location}"
+  resource_group_name = "${azurerm_resource_group.k8s.name}"
+  dns_prefix          = "${var.cluster_name}-dns"
+
+  agent_pool_profile {
+    name    = "${var.pool_name}"
+    count   = "${var.agent_count}"
+    vm_size = "${var.instance_type}"
+  }
+
+  service_principal {
+    client_id     = "${var.client_id}"
+    client_secret = "${var.client_secret}"
+  }
+}
---
+++ terraform/aks/vars.tf
@@ -0,0 +1,27 @@
+variable "resource_group_name" {
+  default = "k8s"
+}
+
+variable "cluster_name" {
+  default = "gltest"
+}
+
+variable "agent_count" {
+  description = "Enter the number of nodes in a pool"
+}
+
+variable "instance_type" {
+  default = "Standard_DS1_v2"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "pool_name" {
+  default = "nodepool1"
+}
+
+variable "client_id" {}
+
+variable "client_secret" {}
